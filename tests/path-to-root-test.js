b0cc51d69984f70ea24dba8ddf77fd1e3c7db4b7
Stefan Penner <stefan.penner@gmail.com>
2017-06-20 09:03:16 -0700

    error if path to root was not found

--- tests/path-to-root-test.js
+++ tests/path-to-root-test.js
@@ -14,7 +14,7 @@ describe('pathToRoot', function() {
 
   describe('walking', function(){
     it('if root has no name, give it one', function() {
-      expect(pathToRoot({ index: 0, in: [] })).to.eql(['root']);
+      expect(pathToRoot({ index: 0, in: [] })[0].name).to.eql('(root)');
     });
 
     it('supports common scenarios', function() {
@@ -38,9 +38,31 @@ describe('pathToRoot', function() {
         ]
       };
 
-      expect(pathToRoot(root)).to.deep.eql(['root']);
-      expect(pathToRoot(one)).to.deep.eql([one, two, 'root']);
-      expect(pathToRoot(two)).to.deep.eql([two, 'root']);
+      expect(pathToRoot(root)).to.deep.eql([root]);
+      expect(pathToRoot(one)).to.deep.eql([one, two, root]);
+      expect(pathToRoot(two)).to.deep.eql([two, root]);
+    });
+
+    it('errors if no path to root can be found', function() {
+      let root = {
+        index: 0,
+        in: [ ]
+      };
+
+      let two = {
+        index: 2,
+        in: [ ],
+        toString() { return '<two:2>'; }
+      };
+
+      let one = {
+        index: 1,
+        in: [ { from: two }, ],
+        toString() { return '<two:1>'; }
+      };
+
+      expect(() => pathToRoot(one)).to.throw(/has no path to root/);
+      expect(() => pathToRoot(two)).to.throw(/has no path to root/);
     });
 
     it('supports skipping specific nodes', function() {
@@ -64,7 +86,7 @@ describe('pathToRoot', function() {
         ]
       };
 
-      expect(pathToRoot(one, new WeakSet([two]))).to.deep.eql([one, 'root']);
+      expect(pathToRoot(one, new WeakSet([two]))).to.deep.eql([one, root]);
     });
   });
 });
